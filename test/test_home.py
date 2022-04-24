a8ef71a5bb5307fe491c113eef872060688daf7a
Stefan Hagen <stefan@hagen.link>
2022-04-24 22:40:00 +0200

    Added tests for home route

---
+++ test/test_home.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from server import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Braces" in response.text
